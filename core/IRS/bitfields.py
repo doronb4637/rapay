2b65352ad50ab9f6c90de2c427e1c0e376529637
doronb4637 <doronbaruch10@gmail.com>
2026-08-16 09:17:58 +0300

    Python 3.10 adjustments

--- core/IRS/bitfields.py
+++ core/IRS/bitfields.py
@@ -1,14 +1,14 @@
 # logic/bitfields.py
 import struct
-from enum import IntEnum, EnumType
+from enum import IntEnum, EnumMeta
 from typing import Any, Callable
-from IRS.buffers import BinaryReader, BinaryWriter
-from IRS.fields import BaseField
-from IRS.constants import *
+from .buffers import BinaryReader, BinaryWriter
+from .fields import BaseField
+from .constants import *
 
 
 def baseType(base_type: int, endian = "<") -> Callable[[type], type]:
-    def wrapper(cls: EnumType | BitFieldMeta) -> EnumType | BitFieldMeta:
+    def wrapper(cls: EnumMeta | BitFieldMeta) -> EnumMeta | BitFieldMeta:
         fmt = {1: UInt8, 2: UInt16, 4: UInt32, 8: UInt64}.get(base_type, UInt8) if isinstance(base_type, int) else base_type
         if isinstance(cls, BitFieldMeta):
             cls._packer_ = struct.Struct(endian + fmt)
